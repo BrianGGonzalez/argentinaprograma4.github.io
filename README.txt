98add61091bf236e8c34fa7b662265ab867e88e8
BrianGGonzalez <126628433+BrianGGonzalez@users.noreply.github.com>
2023-03-30 15:06:32 -0300

    Multiples cambios estéticos y de funcionalidad

---
+++ README.txt
@@ -0,0 +1,3 @@
+Bueno, intente hacer lo mejor que pude con Randomuser. 
+Tuve el problema de que no me termino dando los estudios y trabajos realizados, asi que los escribi a mano.
+Cualquier correccion es bienvenida, muchas gracias.
--- index.js
+++ index.js
@@ -1,84 +1,74 @@
 const url = 'https://randomuser.me/api/';
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const usuario = data.results[0];
-        mostrarDatosUsuario(usuario);
-    })
-    .catch(error => {
-        console.log('Ha ocurrido un error:', error);
-    });
-
+  .then(response => response.json())
+  .then(data => mostrarDatosUsuario(data.results[0]))
+  .catch(error => console.error('Ha ocurrido un error:', error));
 
 function mostrarDatosUsuario(usuario) {
-    const imagenPerfil = document.getElementById('imagen-perfil');
-    imagenPerfil.src = usuario.picture.large;
+  document.getElementById('imagen-perfil').src = usuario.picture.large;
+  document.getElementById('nombre-completo').textContent = `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`;
+  document.getElementById('genero').textContent = usuario.gender === 'male' ? 'Masculino' : 'Femenino';
+  document.getElementById('edad').textContent = usuario.dob.age;
+  document.getElementById('fecha-nacimiento').textContent = new Date(usuario.dob.date).toLocaleDateString();
+  document.getElementById('nacionalidad').textContent = obtenerNombrePais(usuario.nat);
+  document.getElementById('direccion').textContent = `${usuario.location.street.name} ${usuario.location.street.number}`;
+  document.getElementById('codigo-postal').textContent = usuario.location.postcode;
+  document.getElementById('mail').textContent = usuario.email;
+  document.getElementById('telefono').textContent = usuario.phone;
+}
 
-    const nombreCompleto = document.getElementById('nombre-completo');
-    nombreCompleto.textContent = usuario.name.title + ' ' + usuario.name.first + ' ' + usuario.name.last;
+function obtenerNombrePais(abreviatura) {
+  const paises = {
+    AU: 'Australia',
+    BR: 'Brasil',
+    CA: 'Canadá',
+    CH: 'Suiza',
+    DE: 'Alemania',
+    DK: 'Dinamarca',
+    ES: 'España',
+    FI: 'Finlandia',
+    FR: 'Francia',
+    GB: 'Reino Unido',
+    IE: 'Irlanda',
+    IR: 'Irán',
+    NO: 'Noruega',
+    NL: 'Países Bajos',
+    NZ: 'Nueva Zelanda',
+    TR: 'Turquía',
+    US: 'Estados Unidos',
+    IN: 'India',
+    MX: 'Mexico',
+    RS: 'Serbia'
+  };
+  return paises[abreviatura] || abreviatura;
+}
 
-    const genero = document.getElementById('genero');
-    const esMasculino = usuario.gender === 'male';
-    const textoGenero = esMasculino ? 'Género: Masculino' : 'Género: Femenino';
-    genero.textContent = textoGenero;
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  const navbar = document.getElementById("navbar");
+  navbar.style.top = prevScrollPos > currentScrollPos ? "0" : "-60px";
+  prevScrollPos = currentScrollPos;
+};
 
-    const edad = document.getElementById('edad');
-    edad.textContent = 'Edad: ' + usuario.dob.age;
+const content = document.getElementById("content");
+let isOpen = false;
+function toggleContent() {
+  content.style.display = isOpen ? "none" : "block";
+  isOpen = !isOpen;
+}
+content.addEventListener("mouseover", () => content.style.display = "block");
+content.addEventListener("click", toggleContent);
 
-    const fechaNacimiento = document.getElementById('fecha-nacimiento');
-    fechaNacimiento.textContent = 'Fecha de nacimiento: ' + new Date(usuario.dob.date).toLocaleDateString();
+function enviarFormulario() {
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const email = document.getElementById("email").value;
+  const asunto = document.getElementById("asunto").value;
+  const mensaje = document.getElementById("mensaje").value;
 
-    function obtenerNombrePais(abreviatura) {
-        const paises = {
-          AU: 'Australia',
-          BR: 'Brasil',
-          CA: 'Canadá',
-          CH: 'Suiza',
-          DE: 'Alemania',
-          DK: 'Dinamarca',
-          ES: 'España',
-          FI: 'Finlandia',
-          FR: 'Francia',
-          GB: 'Reino Unido',
-          IE: 'Irlanda',
-          IR: 'Irán',
-          NO: 'Noruega',
-          NL: 'Países Bajos',
-          NZ: 'Nueva Zelanda',
-          TR: 'Turquía',
-          US: 'Estados Unidos',
-          IN: 'India',
-          MX: 'Mexico',
-          RS: 'Serbia',
-          AU: 'Australia'
-        };
-      
-        return paises[abreviatura] || abreviatura;
-      }      
+  const mensajeConfirmacion = `Gracias por contactarnos, ${nombre} ${apellido}. Nos comunicaremos con usted a la brevedad.`;
 
-    const nacionalidad = document.getElementById('nacionalidad');
-    nacionalidad.textContent = 'Nacionalidad: ' + obtenerNombrePais(usuario.nat);
-
-    const correoElectronico = document.getElementById('correo-electronico');
-    correoElectronico.textContent = 'Correo electrónico: ' + usuario.email;
-
-    const telefono = document.getElementById('telefono');
-    telefono.textContent = 'Teléfono: ' + usuario.phone
-
-//No me agarro la experiencia laboral y los estudios realizados
-
-   const experienciaLaboral = document.getElementById('experiencia-laboral');
-   usuario.experience.forEach(exp => {
-    const li = document.createElement('li');
-    li.textContent = exp.title + ' en ' + exp.company;
-    experienciaLaboral.appendChild(li);
-    });
-      
-    const estudiosRealizados = document.getElementById('estudios-realizados');
-    usuario.education.forEach(edu => {
-    const li = document.createElement('li');
-    li.textContent = edu.degree + ' en ' + edu.major;
-    estudiosRealizados.appendChild(li);
-    });
+  document.getElementById("mensajeConfirmacion").textContent = mensajeConfirmacion;
 }
